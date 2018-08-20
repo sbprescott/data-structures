fcd3a0190d0a6f2c0e575d66803830558d0d7ed0
Sam Prescott <sprescott3@gmail.com>
2018-08-20 19:23:26 -0400

    implemented readers/writers problem as test of binary semaphore

--- src/synchronization/readers_writers.cc
+++ src/synchronization/readers_writers.cc
@@ -1,24 +1,55 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
 
 #include "binary_semaphore.h"
+
+using namespace std;
 
 BinarySemaphore *reader;
 BinarySemaphore *writer;
 int shared = 0;
 
-void *foo(void *bar) {
-    int *time = static_cast<int*>(bar);
+
+void *read(void *foo) {
+    
+    int *time = static_cast<int*>(foo);
+    
+    while (1) {
+        sleep(3);
+
+        reader->wait();
+        cout << "thread_id:" << syscall(SYS_gettid) << " reading value of shared: " << shared << endl;
+        reader->signal();
+    }
 }
 
-void *bar(void *foo) {
+
+void *write(void *foo) {
     int *time = static_cast<int*>(foo);
+   
+    while (1) { 
+        sleep(3);
+        
+        writer->wait();
+        shared += 2;
+        cout << "thread_id:" << syscall(SYS_gettid) << " updating value of shared: " << shared << endl;
+        writer->signal();
+    }
 }
+
 
 int main() {
     pthread_t t1, t2;
 
-    pthread_create(&t1, NULL, foo, (void*) 1000);
-    pthread_create(&t2, NULL, bar, (void*) 1000);
+    reader = new BinarySemaphore();
+    writer = new BinarySemaphore();
+    
+    cout << "semaphores created" << endl;
+
+    pthread_create(&t1, NULL, read, (void*) 3);
+    pthread_create(&t2, NULL, write, (void*) 3);
 
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
