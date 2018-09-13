ee988191b01beff3b7d2b0cf74246a2f41264823
Sam Prescott <sprescott3@gmail.com>
2018-09-12 22:12:29 -0400

    counting semaphore

---
+++ src/synchronization/counting_semaphore.cc
@@ -0,0 +1,42 @@
+// counting-semaphore.cc
+// ------------------------------------------------
+//
+// C++ implementation of counting semaphore using custom
+// binary semaphore. 
+//
+// 
+// @ author: Sam Prescott
+//
+
+#include "counting_semaphore.h"
+
+void CS::wait() {
+    mutex_.wait();  //grab the mutex
+    value_--;
+    
+    if (value_ >= 0) {  // if value_ >= 0, don't go on to waitq_, return
+        mutex_.signal();
+        return;
+    }
+    
+    mutex_.signal();  // release mutex before adding thread to waitq_
+    waitq_.wait();
+    throttle_.signal();  // released thread signals handshake
+
+}
+
+void CS::signal() {
+    mutex_.wait();  // grab mutex
+    value_++;
+    
+    if (value_ > 0) {  // if current value > 0, no threads released
+        mutex_.signal();  // release mutex
+        return;
+    }
+    
+    waitq_.signal();
+    throttle_.wait();
+    mutex_.signal();
+
+}
+
