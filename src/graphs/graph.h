72065803cb973f08c8b8cc2987e4e59a6fa386a1
Sam Prescott <sbp9as@virginia.edu>
2018-11-06 18:01:53 -0500

    expanding graph template

--- src/graphs/graph.h
+++ src/graphs/graph.h
@@ -9,16 +9,17 @@
 #define GRAPH_H
 
 #include <map>
+#include <set>
 
 template<typename t1, typename t2>
 class Graph {
     public:
         Graph() : adj_list_() {}
         ~Graph() {}
-        void add_edge();
+        void add_edge(t1, t2);
     
     private:
-        std::map<t1, t2> adj_list_;
+        std::map<t1, std::set<t2>> adj_list_;
 };
 
 #endif
