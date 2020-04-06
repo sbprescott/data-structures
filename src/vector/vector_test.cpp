b1f23268c50a6cc42e4dbaceeb77be702d1cc5cf
sbprescott <sbp9as@virginia.edu>
2020-04-06 16:59:13 -0400

    fixing style issues

--- src/vector/vector_test.cpp
+++ src/vector/vector_test.cpp
@@ -8,7 +8,7 @@
 TEST_CASE( "Vector default constructor")
 {
     Vector v;
-    REQUIRE( v.size() == 0 );
+    REQUIRE(v.size() == 0);
 }
 
 TEST_CASE("Vector constructor with size")
@@ -16,15 +16,15 @@ TEST_CASE("Vector constructor with size")
     Vector v(2);
     v[0] = 1;
     v[1] = 2;
-    REQUIRE( (v[0] == 1 && v[1] == 2) );
-    REQUIRE( v.size() == 2 );
+    REQUIRE((v[0] == 1 && v[1] == 2));
+    REQUIRE(v.size() == 2);
 }
 
 TEST_CASE("Vector initializer list constructor")
 {
     Vector v({1,2});
-    REQUIRE( (v[0] == 1 && v[1] == 2) );
-    REQUIRE( v.size() == 2 );   
+    REQUIRE((v[0] == 1 && v[1] == 2));
+    REQUIRE(v.size() == 2);   
 }
 
 TEST_CASE("Vector copy constructor")
@@ -32,6 +32,6 @@ TEST_CASE("Vector copy constructor")
     Vector v1;
     Vector v2({1,2});
     Vector v3(v2);
-    REQUIRE( v1 != v2 );
-    REQUIRE( v2 == v3 );
+    REQUIRE(v1 != v2);
+    REQUIRE(v2 == v3);
 }
