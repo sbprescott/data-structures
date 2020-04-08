8a77dcdf2b5b1fd32474f15e41641c8b2bbda9a7
sbprescott <sbp9as@virginia.edu>
2020-04-07 23:07:37 -0400

    removed failing test

--- src/vector/vector_test.cpp
+++ src/vector/vector_test.cpp
@@ -111,9 +111,3 @@ TEST_CASE("push_back()")
     REQUIRE(v2.capacity() == 2);
     REQUIRE(v2[1] == Vector({1,2}));
 }
-
-TEST_CASE("this test should fail")
-{
-    Vector v({1});
-    REQUIRE(v.size() == 0);
-}
